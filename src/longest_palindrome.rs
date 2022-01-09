cd3d1e244330f34c03106aa968fd8cb73c48b64b
alngo <alxengo.pro@gmail.com>
2022-01-09 22:35:37 +0100

    [Ex05] Clean longest palindrome.

--- src/longest_palindrome.rs
+++ src/longest_palindrome.rs
@@ -1,41 +1,35 @@
+use std::iter::Zip;
+use std::iter::Rev;
+use std::slice::Iter;
+
 pub fn longest_palindrome(s: String) -> String {
     let chars: Vec<char> = s.chars().collect();
     let mut res: Option<String> = None;
 
     for i in 0..chars.len() {
-        // for odd length palindrome
         let left = (0..=i).collect::<Vec<usize>>();
         let right = (i..chars.len()).collect::<Vec<usize>>();
         let radius = left.iter().rev().zip(right.iter());
-        for (l, r) in radius {
-            if chars[*l] != chars[*r] {
-                break;
-            }
-            if res.is_none() {
-                res = Some(chars[*l..=*r].iter().collect::<String>());
-            }
-            if res.as_ref().unwrap().len() < *r - *l + 1 {
-                res = Some(chars[*l..=*r].iter().collect::<String>());
-            }
-        }
+        store_palindrome_from_radius(&chars, &mut res, radius);
 
-        // for even length palindrome
         let left = (0..=i).collect::<Vec<usize>>();
         let right = (i + 1..chars.len()).collect::<Vec<usize>>();
         let radius = left.iter().rev().zip(right.iter());
-        for (l, r) in radius {
-            if chars[*l] != chars[*r] {
-                break;
-            }
-            if res.is_none() {
-                res = Some(chars[*l..=*r].iter().collect::<String>());
-            }
-            if res.as_ref().unwrap().len() < *r - *l + 1 {
-                res = Some(chars[*l..=*r].iter().collect::<String>());
-            }
-        }
+        store_palindrome_from_radius(&chars, &mut res, radius);
     }
     res.unwrap_or(String::new())
+}
+
+fn store_palindrome_from_radius(chars: &Vec<char>, res: &mut Option<String>, radius: Zip<Rev<Iter<usize>>, Iter<usize>>) {
+    for (l, r) in radius {
+        if chars[*l] != chars[*r] {
+            break;
+        } else if res.is_none() {
+            *res = Some(chars[*l..=*r].iter().collect::<String>());
+        } else if res.as_ref().unwrap().len() < *r - *l + 1 {
+            *res = Some(chars[*l..=*r].iter().collect::<String>());
+        }
+    }
 }
 
 #[cfg(test)]
