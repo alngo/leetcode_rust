abcc645d243a2f15c2d3e8b77abc846c447d662b
alngo <alxengo.pro@gmail.com>
2022-01-19 21:53:03 +0100

    [Ex10] Add is_match.

--- src/is_match.rs
+++ src/is_match.rs
@@ -1,6 +1,23 @@
-pub fn is_match(_s: String, _p: String) -> bool {
-    return false
+pub fn is_match(s: String, p: String) -> bool {
+    is_match_slice(s.as_bytes(), p.as_bytes())
 }
+
+fn is_match_slice(s: &[u8], p: &[u8]) -> bool {
+    match (p, s) {
+        ([x, b'*', _subp @ ..], [y, subs @ ..]) if *x == b'.' || x == y => {
+            is_match_slice(subs, p)
+        }
+        ([_, b'*', subp @ ..], _) => {
+            is_match_slice(s, subp)
+        }
+        ([x, subp @ ..], [y, subs @ ..]) if *x == b'.' || x == y => {
+            is_match_slice(subs, subp)
+        }
+        ([], s) => s.is_empty(),
+        _ => false,
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -20,4 +37,35 @@ mod tests {
     fn test_is_match_3() {
         assert_eq!(is_match(String::from("aa"), String::from(".*")), true);
     }
+
+    #[test]
+    fn test_is_match_4() {
+        assert_eq!(is_match(String::from("aaa"), String::from("a*")), true);
+    }
+
+    #[test]
+    fn test_is_match_5() {
+        assert_eq!(is_match(String::from("ab"), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_is_match_6() {
+        assert_eq!(is_match(String::from(""), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_is_match_7() {
+        assert_eq!(is_match(String::from(" "), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_is_match_8() {
+        assert_eq!(is_match(String::from(" bc"), String::from(".*b")), false);
+    }
+
+    #[test]
+    fn test_is_match_9() {
+        assert_eq!(is_match(String::from("a"), String::from("a*")), true);
+    }
+
 }
--- src/my_atoi.rs
+++ src/my_atoi.rs
@@ -6,21 +6,22 @@ pub fn my_atoi(s: String) -> i32 {
 
     if chars.len() > 0 {
         let mut i = 0;
+
         if chars[i] == '-' || chars[i] == '+' {
             mult = if chars[i] == '-' { -1 } else { 1 };
             i += 1;
         }
         while i < chars.len() {
-            let digit = find_digits(chars[i], base);
-            if digit.is_some() {
-                let limit: Option<i32> = find_limit(num, *digit.as_ref().unwrap(), mult, base);
-                if limit.is_some() {
-                    return limit.unwrap();
-                }
-                num = num * base + digit.unwrap()
-            } else {
+            let digit: Option<i32> = find_digits(chars[i], base);
+            if digit.is_none() {
                 break;
             }
+
+            let limit: Option<i32> = find_limit(num, digit.unwrap(), mult, base);
+            if limit.is_some() {
+                return limit.unwrap();
+            }
+            num = num * base + digit.unwrap();
             i += 1;
         }
     }
@@ -29,6 +30,7 @@ pub fn my_atoi(s: String) -> i32 {
 
 fn find_digits(c: char, base: i32) -> Option<i32> {
     let cypher: Vec<char> = "0123456789abcdef".chars().collect();
+
     if base <= 16 {
         for i in 0..base {
             if c == cypher[i as usize] {
@@ -40,10 +42,14 @@ fn find_digits(c: char, base: i32) -> Option<i32> {
 }
 
 fn find_limit(num: i32, digit: i32, mult: i32, base: i32) -> Option<i32> {
-    let max_divided: i32 = i32::MAX / 10;
-    let max_digit: i32 = i32::MAX % 10;
+    let max_divided: i32 = i32::MAX / base;
+    let max_digit: i32 = i32::MAX % base;
+
     if num > max_divided || (num == max_divided && digit > max_digit) {
-        return if mult == 1 { Some(i32::MAX)} else { Some(i32::MIN) }
+        return match mult {
+            1 => Some(i32::MAX),
+            _ => Some(i32::MIN)
+        }
     }
     None
 }
